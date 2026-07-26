a9c57235d486a5a361e3c2cac3f60a2d4f0d947b
ZXLee <admin@zxlee.cn>
2026-07-26 23:27:47 +0800

    fix(admin): 异常兼容处理

--- admin/src/components/GlobalFooter/index.vue
+++ admin/src/components/GlobalFooter/index.vue
@@ -1,23 +1,68 @@
 <template>
-  <global-footer class="footer custom-render">
-    <!-- <template v-slot:links>
-      <a href="https://www.github.com/vueComponent/pro-layout" target="_blank">Pro Layout</a>
-      <a href="https://www.github.com/vueComponent/ant-design-vue-pro" target="_blank">Github</a>
-      <a href="https://www.github.com/sendya/" target="_blank">@Sendya</a>
-    </template>
-    <template v-slot:copyright>
-      <a href="https://github.com/vueComponent" target="_blank">vueComponent</a>
-    </template> -->
-  </global-footer>
+  <div class="footer custom-render">
+    <div class="ant-pro-global-footer">
+      <div v-if="$slots.links || (links && links.length)" class="ant-pro-global-footer-links">
+        <slot name="links">
+          <template v-if="Array.isArray(links)">
+            <a
+              v-for="link in links"
+              :key="link.key || link.title"
+              :title="link.title"
+              :target="link.blankTarget ? '_blank' : '_self'"
+              :href="link.href"
+            >{{ link.title }}</a>
+          </template>
+        </slot>
+      </div>
+      <div v-if="$slots.copyright || copyright" class="ant-pro-global-footer-copyright">
+        <slot name="copyright">{{ copyright }}</slot>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { GlobalFooter } from '@ant-design-vue/pro-layout'
-
 export default {
   name: 'ProGlobalFooter',
-  components: {
-    GlobalFooter
+  props: {
+    links: {
+      type: Array,
+      default: () => []
+    },
+    copyright: {
+      type: String,
+      default: ''
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+.footer {
+  padding: 0 16px;
+  margin: 48px 0 24px 0;
+  text-align: center;
+
+  .ant-pro-global-footer-links {
+    margin-bottom: 8px;
+
+    a {
+      color: rgba(0, 0, 0, 0.45);
+      transition: all 0.3s;
+
+      &:not(:last-child) {
+        margin-right: 40px;
+      }
+
+      &:hover {
+        color: rgba(0, 0, 0, 0.65);
+      }
+    }
+  }
+
+  .ant-pro-global-footer-copyright {
+    color: rgba(0, 0, 0, 0.45);
+    font-size: 14px;
+  }
+}
+</style>
